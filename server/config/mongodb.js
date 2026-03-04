9aade02623e63aa70fd462f4885a065afce343fe
Satya Sri <satyakattamuri05@gmail.com>
2026-03-04 19:51:45 +0530

    mogodb change

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  try {
     mongoose.connection.on("connected", () => {
-        console.log("MongoDB connected successfully");
+      console.log("MongoDB connected successfully");
     });
-    await mongoose.connect("mongodb://localhost:27017/mern-auth");
-}
+
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
