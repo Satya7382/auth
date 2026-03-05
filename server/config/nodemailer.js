d999ff014fabf6f01462e628bb2e104d63daf2f8
Satya Sri <satyakattamuri05@gmail.com>
2026-03-05 19:51:38 +0530

    change 8

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -6,8 +6,9 @@ const transporter = nodemailer.createTransport({
     secure: true,
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
+        pass: process.env.SMTP_PASS,
+    },
+    connectionTimeout: 10000, 
 });
 
 export default transporter;
