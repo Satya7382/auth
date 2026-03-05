751b6dc3a24366d9e770407b5bbfef63f4952a65
Satya Sri <satyakattamuri05@gmail.com>
2026-03-05 20:37:53 +0530

    change 11

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, message) => {
     try {
         await resend.emails.send({
-            from: 'satyakattamuri05@gmail.com',
+            from: process.env.SENDER_EMAIL,
             to: to,
             subject: subject,
             text: message,
