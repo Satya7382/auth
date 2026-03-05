1ccfec0596c1ec2afbdbddcd6a077a7c1279d509
Satya Sri <satyakattamuri05@gmail.com>
2026-03-05 19:41:07 +0530

    change 7

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,12 +1,12 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS
     }
 });
 
