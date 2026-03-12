65fe48c90686fb808a826d18af6f9259f7e6b806
Satya7382 <satyakattamuri05@gmail.com>
2026-03-12 06:38:48 +0530

    Switch email service from Resend to SendGrid

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,17 +1,19 @@
-import { Resend } from 'resend';
+import sgMail from "@sendgrid/mail";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async (to, subject, message) => {
+const sendEmail = async (to, subject, text) => {
     try {
-        await resend.emails.send({
-            from: 'onboarding@resend.dev',
-            to: to,
-            subject: subject,
-            text: message,
+        await sgMail.send({
+            to,
+            from: process.env.EMAIL_FROM,
+            subject,
+            text,
+            html: `<p>${text.replace(/\n/g, "<br>")}</p>`
         });
+        console.log("Email sent successfully");
     } catch (error) {
-        console.log("Email Error:", error);
+        console.error(error);
     }
 };
 
